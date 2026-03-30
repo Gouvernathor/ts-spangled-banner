a7537b398d578446d9893beb3cdb493d9b46e119
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-03-30 22:30:46 +0200

    provide a preferred single-options-parameter overload for the main index function

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,37 @@ import { Measurements } from "./geometry.js";
 import { findBestStarLayout, LayoutKind } from "./stars.js";
 import { FlagColors, getSVGFromLayout } from "./svg.js";
 
+interface GetSVGParams {
+    cantonFactor: number;
+    kinds: LayoutKind[];
+    nStripes: number;
+    proportionalStarSize: boolean;
+    width: number|string;
+    height: number|string;
+    colors: FlagColors;
+}
+
+/**
+ * This is the preferred overload.
+ */
+export function getSVG(nStars: number,
+    options: Partial<Readonly<GetSVGParams>>,
+): SVGSVGElement;
+/**
+ * @deprecated
+ * This version is deprecated. Please use the overload with the single options object instead.
+ */
+export function getSVG(nStars: number,
+    FBSLParams?: { cantonFactor?: number, kinds?: readonly LayoutKind[] },
+    MGParams?: { nStripes?: number, proportionalStarSize?: boolean },
+    GSFLParams?: { width?: number|string, height?: number|string, colors?: FlagColors },
+): SVGSVGElement;
 export function getSVG(nStars: number,
     FBSLParams?: { cantonFactor?: number, kinds?: readonly LayoutKind[] },
     MGParams?: { nStripes?: number, proportionalStarSize?: boolean },
     GSFLParams?: { width?: number|string, height?: number|string, colors?: FlagColors },
 ): SVGSVGElement {
     const layout = findBestStarLayout(nStars, FBSLParams);
-    const measurements = Measurements.generate({ starLayout: layout, ...MGParams });
-    return getSVGFromLayout(measurements, layout, GSFLParams);
+    const measurements = Measurements.generate({ starLayout: layout, ...FBSLParams, ...MGParams });
+    return getSVGFromLayout(measurements, layout, { ...FBSLParams, ...GSFLParams });
 }
