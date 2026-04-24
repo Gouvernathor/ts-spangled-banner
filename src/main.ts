14cf85e6eef2bf947480f3700a1e63b3dad74828
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-04-24 07:29:34 +0200

    change the input signature, allowing no number of stars to be passed

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { Measurements, MGOptions } from "./geometry.js";
-import { FBSLOptions, findBestStarLayout } from "./stars.js";
+import { DEFAULT_LAYOUT, FBSLOptions, findBestStarLayout } from "./stars.js";
 import { getSVGFromLayout, GSVGOptions } from "./svg.js";
 
-interface GetSVGOptions extends FBSLOptions, Omit<MGOptions, "starLayout">, GSVGOptions {
+const DEFAULT_N_STARS = DEFAULT_LAYOUT[0]*DEFAULT_LAYOUT[1] + DEFAULT_LAYOUT[2]*DEFAULT_LAYOUT[3];
+
+interface GetSVGOptions extends FBSLOptions, MGOptions, GSVGOptions {
+    nStars: number;
 }
 
-export function getSVG(nStars: number,
-    options?: Partial<Readonly<GetSVGOptions>>,
-): SVGSVGElement {
-    const layout = findBestStarLayout(nStars, options);
+export function getSVG(options?: Partial<Readonly<GetSVGOptions>>): SVGSVGElement {
+    const layout = options?.starLayout ??
+        findBestStarLayout(options?.nStars ?? DEFAULT_N_STARS, options);
     const measurements = Measurements.generate({ starLayout: layout, ...options });
     return getSVGFromLayout(measurements, layout, options);
 }
